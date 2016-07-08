c2d0d10c91e02a9a56a29401b17b9965e0e087bf
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-07-09 02:48:58 +0300

    Datastore fix

--- bin/main.sh
+++ bin/main.sh
@@ -770,6 +770,8 @@ sudo -u $USER_NAME createdb -E UTF8 geonode_live_app
 sudo -u $USER_NAME psql geonode_live_app -c 'create extension postgis;'
 sudo -u $USER_NAME createdb -E UTF8 geonode_live
 sudo -u $USER_NAME psql geonode_live -c 'create extension postgis;'
+sudo -u $USER_NAME psql geonode_live -c 'GRANT ALL ON geometry_columns TO PUBLIC;'
+sudo -u $USER_NAME psql geonode_live -c 'GRANT ALL ON spatial_ref_sys TO PUBLIC;'
 
 sudo -u "$USER_NAME" mkdir -p "$USER_HOME"/.virtualenvs/geonode_live/local/lib/python2.7/site-packages/geonode/static
 cd "$USER_HOME"/geonode_live
