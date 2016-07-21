cb59eade7b086686da2be0ae3eca976e5f86215f
Angelos Tzotsos <gcpp.kalxas@gmail.com>
2016-07-21 04:07:38 +0300

    Disable import handlers for now

--- conf/geonode/local_settings.py
+++ conf/geonode/local_settings.py
@@ -60,5 +60,6 @@ OSGEO_IMPORTER_GEONODE_ENABLED = True
 #LOGGING['loggers']['osgeo_importer'] = {"handlers": ["console"], "level": "DEBUG"}
 #DATABASE_ROUTERS = ['osgeo_importer_prj.dbrouters.DefaultOnlyMigrations']
 
-IMPORT_HANDLERS = ['osgeo_importer.handlers.FieldConverterHandler',
-                           'osgeo_importer.handlers.geonode.GeoNodePublishHandler']
+IMPORT_HANDLERS = []
+#IMPORT_HANDLERS = ['osgeo_importer.handlers.FieldConverterHandler',
+#                           'osgeo_importer.handlers.geonode.GeoNodePublishHandler']
